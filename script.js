789ecb8d3d3645efcbb068c728e2ed742796923f
Tiatemsu Longkumer <tiatemsu.l.k.r8@gmail.com>
2026-05-10 22:32:44 +0600

    Add smooth scrolling for anchor links

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", () => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (!target) return;
+    target.setAttribute("tabindex", "-1");
+    target.focus({ preventScroll: true });
+  });
+});
